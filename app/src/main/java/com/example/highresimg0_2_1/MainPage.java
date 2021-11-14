95981b78fd29699de372c960fe0d6d866eb38cf7
itsnabil4012 <nabilpatel4012@gmail.com>
2021-11-14 22:35:42 +0530

    initial changes

---
+++ app/src/main/java/com/example/highresimg0_2_1/MainPage.java
@@ -0,0 +1,14 @@
+package com.example.highresimg0_2_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
